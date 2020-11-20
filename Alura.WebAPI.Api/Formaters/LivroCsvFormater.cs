7c5b0135ceef43b9c247fc3b0c4c8761212bef95
karoline.lopes <karoline.silva7@fatec.sp.gov.br>
2020-11-20 12:39:58 -0300

    Web API

---
+++ Alura.WebAPI.Api/Formaters/LivroCsvFormater.cs
@@ -0,0 +1,48 @@
+﻿using Alura.ListaLeitura.Modelos;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.Api.Formaters
+{
+    public class LivroCsvFormater : TextOutputFormatter
+    {
+        public LivroCsvFormater()
+        {
+            var textCsvMediaType = MediaTypeHeaderValue.Parse("text/csv");
+            var appCsvMediaType = MediaTypeHeaderValue.Parse("application/csv");
+            SupportedMediaTypes.Add(textCsvMediaType);
+            SupportedMediaTypes.Add(appCsvMediaType);
+            SupportedEncodings.Add(Encoding.UTF8);
+        }
+
+        protected override bool CanWriteType(Type type)
+        {
+            return type == typeof(LivroApi);
+        }
+
+        public override Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
+        {
+            var livroCsv = "";
+
+            if (context.Object is LivroApi)
+            {
+                var livro = context.Object as LivroApi;
+
+                livroCsv = $"{livro.Titulo};{livro.Subtitulo};{livro.Autor};{livro.Lista}";
+            }
+
+
+            using (var escritor = context.WriterFactory(context.HttpContext.Response.Body, selectedEncoding))
+            {
+                return escritor.WriteAsync(livroCsv);
+            }
+
+                //escritor.Close();
+        }
+    }
+}
